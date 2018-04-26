972902d4cd84b7413961c0bc29127d583b6780a0
crazygirlcoder <sharmishtaroy21@gmail.com>
2018-04-26 16:05:46 -0400

    needleinahaystack

---
+++ NeedleInAHaystack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedleInAHaystack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var bob =StrStr("aerttyljsdgvsjgsjdgsjdhgjshsjadhgfjdshgfjsgfdsafhzzz", "zzz");
+            int StrStr( string haystack, string needle)
+            {
+                var haystackArr = haystack.ToCharArray();
+                var needleArr = needle.ToCharArray();
+                for(int i = 0; i< haystackArr.Length; i++)
+                {
+                    for(int j = 0; j < needleArr.Length; j++)
+                    {
+                        if(needleArr[j]== haystackArr[i])
+                        {
+                            return i;
+                        }
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
